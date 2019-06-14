69636c29a1df55652e6b596263bbe56ae247c897
陈俊嘉 <cjj19970505@live.cn>
2019-06-14 08:42:31 +0800

    添加项目文件。

---
+++ VR33B/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VR33B
+{
+    public class Class1
+    {
+    }
+}
