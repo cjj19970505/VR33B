ec4d14ae281935310f475480620ae4f78a072441
陈俊嘉 <cjj19970505@live.cn>
2019-06-14 13:59:54 +0800

    sdf

---
+++ VR33B.LineGraphicTest/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using InteractiveDataDisplay.WPF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VR33B.LineGraphicTest
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            double[] x = new double[200];
+            for (int i = 0; i < x.Length; i++)
+                x[i] = 3.1415 * i / (x.Length - 1);
+            for(int i = 0;i<25;i++)
+            {
+                var lg = new LineGraph();
+                Lines.Children.Add(lg);
+                lg.Stroke = new SolidColorBrush(Color.FromArgb(255, 0, 0, 0));
+                lg.Description = "FUCK";
+                lg.StrokeThickness = 2;
+                lg.Plot(x, x.Select(v => Math.Sin(v + i / 10.0)).ToArray());
+            }
+        }
+    }
+}
