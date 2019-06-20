0094f982d87dc3b5e587a6963d96a2ff57195254
陈俊嘉 <cjj19970505@live.cn>
2019-06-20 21:46:09 +0800

    ddd

---
+++ VR33B.LineGraphic/VR33BOxyLegendControl.xaml.cs
@@ -0,0 +1,39 @@
+﻿using OxyPlot.Series;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VR33B.LineGraphic
+{
+    /// <summary>
+    /// VR33BOxyLegendControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class VR33BOxyLegendControl : UserControl
+    {
+        public LineSeries BindingLineSeries
+        {
+            get
+            {
+                return DataContext as LineSeries;
+            }
+        }
+        public VR33BOxyLegendControl()
+        {
+            InitializeComponent();
+            
+        }
+
+
+    }
+}
--- VR33B.LineGraphic/VisibilityToCheckedConverter.cs
+++ VR33B.LineGraphic/VisibilityToCheckedConverter.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using OxyPlot;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Data;
+using System.Windows.Media;
 
 namespace VR33B.LineGraphic
 {
@@ -18,6 +21,21 @@ namespace VR33B.LineGraphic
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
             return ((bool)value) ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+
+    public class OxyColorToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var oxyColor = (OxyColor)value;
+            return Color.FromArgb(oxyColor.A, oxyColor.R, oxyColor.G, oxyColor.B);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var color = (Color)value;
+            return OxyColor.FromArgb(color.A, color.R, color.G, color.B);
         }
     }
 }
